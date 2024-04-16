c566a1c278d4ba3d6eadac57dea43e1dbf1eba63
Tim Zenner <epidemicz@gmail.com>
2024-04-15 23:43:16 -0500

    default serve directory to be null
    - default serve directory to be null so wwwroot will be used by default

--- src/ConfigOptions.cs
+++ src/ConfigOptions.cs
@@ -3,7 +3,7 @@ using System.Security.Cryptography.X509Certificates;
 public class ConfigOptions
 {
     public string? ServerPort { get; set; } = "8085";
-    public string? ServeDirectory { get; set; } = Directory.GetCurrentDirectory();
+    public string? ServeDirectory { get; set; } = null;
     public bool UseDirectoryBrowser { get; set; } = true;
     public bool UseRequiredGodotHeaders { get; set; } = true;
 
